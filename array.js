e43cfcdd81042062b49291d537186f8fe89fbf33
RibkaNathasya <70162184+RibkaNathasya@users.noreply.github.com>
2020-11-18 14:47:45 +0800

    Pertemuan24
    Array

---
+++ array.js
@@ -0,0 +1,88 @@
+//Array
+
+// let nilai1 = 85;
+// let nilai2 = 90;
+// let nilai3 = 80;
+// let nilai4 = 75;
+// let nilai5 = 100;
+
+// let nilai = [85, 90, 80, 75, 100];
+// nilai[2] = 90;
+// console.log(nilai[nilai.length-1]);
+// console.log(nilai);
+
+// let john = ["John", "Doe", 33, true];
+// console.log(john[john.length-1]);
+// console.log(john);
+
+// let nilai = [85, 90, 80, 75, 100];
+// for(let i=0; i<nilai.length; i++){
+// console.log("Nilai = ",nilai[i]+5);
+// }
+
+// console.log(nilai);
+
+//toString()
+// let john = ["John", "Doe", 33, true];
+// console.log(john[john.length-1]);
+// console.log(john);
+// console.log(john.toString());
+
+//join()
+// console.log(john.join(" - "));
+
+//pop()
+// john.pop();
+// console.log(john.join(" - "));
+
+//push()
+// john.push(true);
+// john.push("Hello");
+// console.log(john.join(" - "));
+
+//Unshift()
+// john.unshift("Prof.");
+// console.log(john);
+
+//Shift()
+// john.shift();
+// console.log(john);
+
+//splice()
+// let kelas = ["Meja", "PapanTulis", "Rak", "Kursi"];
+// kelas.splice(3, 0, "Meja", "Pensil");
+// console.log(Kelas);
+
+//concat()
+// let buah = ["Pepaya", "Nenas", "Kelapa"];
+// let sayur = ["Brokoli", "Bayam", "Toge"];
+// let biji = ["Kedelai", "Kacang Almond", "Kacang Hijau"];
+// let makanan = buah.concat(sayur, biji);
+// console.log(makanan);
+
+//slice()
+// let kelaaIPA = kelas.slice(3);
+// console.log(kelasIPA);
+// let kelasIPA = kelas.slice(0, 3);
+// console.log(kelasIPA);
+
+//sort()
+// buah.sort();
+// console.log(buah);
+// kamar.sort()
+// console.log(kelas);
+// nilai.sort()
+// console.log(nilai);
+// let nama = ["Tasya", "Ribka", "Niall", "Joy", "Harry"];
+// nama.sort();
+// console.log(nama);
+// let tahunLahir = [2001, 2001, 1993, 1995, 1994];
+// tahunLahir.sort();
+// console.log(tahunLahir);
+
+//reverse()
+// let nama = ["Tasya", "Ribka", "Niall", "Joy", "Harry"];
+// nama.reverse();
+// console.log(nama);
+// nama.sort().reverse();
+// console.log(nama);
